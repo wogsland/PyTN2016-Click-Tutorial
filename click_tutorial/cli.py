40be3a970f91293927c44b9d7536ef62d96c7fb0
Bradley Wogsland <bradley@wogsland.org>
2016-02-07 14:06:27 -0600

    lesson 001 passing

--- click_tutorial/cli.py
+++ click_tutorial/cli.py
@@ -1,11 +1,20 @@
 import click
 
 @click.command(name='pytn')
-def cli():
+@click.option('--single', '-s')
+@click.option('--multi',  multiple=True)
+@click.option('--enable-feature/--no-enable-feature')
+@click.option('--flag',  is_flag=True)
+@click.option('--verbose', '-v',  count=True)
+def cli(single, multi, enable_feature, flag, verbose):
     """
     Output a greeting to PyTennessee!
     """
-    click.echo("Hello, PyTN!")
-
+    click.echo("single: {0}".format(single))
+    for m in multi:
+        click.echo("multi: {0}".format(m))
+    click.echo("enable_feature: {0}".format(enable_feature))
+    click.echo("flag: {0}".format(flag))
+    click.echo("verbose: {0}".format(verbose))
 if __name__ == '__main__':
     cli()
